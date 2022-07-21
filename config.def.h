87d34777472861b447b5a84d746f34d4f0c630fd
DemonKingSwarn <rockingswarn@gmail.com>
2022-07-21 22:02:44 +0530

    source code added

---
+++ config.def.h
@@ -0,0 +1,20 @@
+static const char *background_color = "#282a36";
+static const char *border_color = "#ececec";
+static const char *font_color = "#6272a4";
+static const char *font_pattern = "JetBrains Mono:size=11.5";
+static const unsigned line_spacing = 5;
+static const unsigned int padding = 15;
+static const int use_primary_monitor = 0;
+
+static const unsigned int width = 300;
+static const unsigned int border_size = 2;
+static const unsigned int pos_x = 30;
+static const unsigned int pos_y = 60;
+
+enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
+enum corners corner = TOP_RIGHT;
+
+static const unsigned int duration = 5; /* in seconds */
+
+#define DISMISS_BUTTON Button1
+#define ACTION_BUTTON Button3
